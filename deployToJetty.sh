62382fe234b991d724e9de791e97de8a3a92a566
Tim Pizey <tim.pizey@oucs.ox.ac.uk>
2009-05-19 14:20:07 +0000

    Copy from deployed location
    git-svn-id: https://svn.oucs.ox.ac.uk/projects/erewhon/uriinterface@425 98330f3f-1342-47a8-b5c6-0a1dd6e8dbeb

--- deployToJetty.sh
+++ deployToJetty.sh
@@ -2,5 +2,6 @@
 #  Copy deployed war to jetty
 #  19/05/2009 TPP
 #
+#  This should be filtered so that versions do not need to be manually changed
 #
-cp target/uriinterface-1.0.2.war /usr/share/jetty6/webapps/OxPointsUriGui.war
+cp /var/www/maven2/uk/ac/ox/oucs/erewhon/uriinterface/1.0.2/uriinterface-1.0.2.war /usr/share/jetty6/webapps/OxPointsUriGui.war
